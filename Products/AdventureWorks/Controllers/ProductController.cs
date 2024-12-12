8d2d292c277da68345294142025329f1e8460ef0
Nathaniel Tobing <nathanieltobing@gmail.com>
2024-12-12 21:23:15 +0700

    new

---
+++ Products/AdventureWorks/Controllers/ProductController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdventureWorks.Models;
+using AdventureWorks.Models.Infrastructure;
+using AdventureWorks.Models.Services;
+
+namespace AdventureWorks.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+        // GET: Product
+
+        public ProductController() : this(new ProductRepository())
+        {
+
+        }
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public ActionResult Index()
+        {
+            //var products = _repository.GetProducts();
+            return View();
+            
+        }
+        public JsonResult GetProduct()
+        {
+            var data = _repository.GetProducts();
+
+            return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _repository.InsertProduct(product);
+                    return RedirectToAction("Index");
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again," +
+                    "and if the problem persists see your system administrator");
+            }
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProductModel model = _repository.GetProductByID(id);
+            return View(model);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ProductModel product)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    _repository.UpdateProduct(product);
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the " +
+                    "problem persists see your system administrator.");
+                
+            }
+            return View(product);
+        }     
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                _repository.deleteProduct(id);
+                return RedirectToAction("Index");
+                //Publisher publisher = context.Publishers.Where(x => x.Id == publishInput.Id).SingleOrDefault();
+                //context.Publishers.DeleteOnSubmit(publisher);
+                //context.SubmitChanges();
+                //return RedirectToAction("Index");
+            }
+            catch
+            {
+                Response.StatusCode = 500;
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the " +
+                   "problem persists see your system administrator.");
+                return View();
+            }
+        }
+    }
+}
---
+++ Products/AdventureWorks/Models/Services/ProductCategoryRepository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using AdventureWorks.Models.Infrastructure;
+
+namespace AdventureWorks.Models.Services
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private OperationDataContext _dataContext;
+        public string connectionStringSettings = ConfigurationManager.ConnectionStrings["AdventureWorks2022ConnectionString2"].ToString();
+
+        public string ConnectionStringSettings
+        {
+            get => connectionStringSettings; set =>
+                connectionStringSettings = value;
+        }
+        public ProductCategoryRepository()
+        {
+            _dataContext = new OperationDataContext(ConnectionStringSettings);
+        }
+        public void deleteProductCategory(int ProductCategoryId)
+        {
+            ProductCategory productCategory = _dataContext.ProductCategories.Where(u => u.ProductCategoryID == ProductCategoryId).SingleOrDefault();
+            _dataContext.ProductCategories.DeleteOnSubmit(productCategory);
+            _dataContext.SubmitChanges();
+        }
+
+        public IEnumerable<ProductCategoryModel> GetProductCategory()
+        {
+            IList<ProductCategoryModel> productCategoryList = new List<ProductCategoryModel>();
+            var query = from category in _dataContext.ProductCategories
+                        select category;
+                        
+            var productCategories = query.ToList();
+            foreach (var categoryData in productCategories)
+            {
+                productCategoryList.Add(new ProductCategoryModel()
+                {
+                    ProductCategoryID = categoryData.ProductCategoryID,
+                    Name = categoryData.Name,
+                    ModifiedDate = categoryData.ModifiedDate
+                    
+                });
+            }
+            return productCategoryList;
+        }
+
+        public ProductCategoryModel GetProductCategoryByID(int ProductCategoryId)
+        {
+            var query = from u in _dataContext.ProductCategories
+                        where
+                        u.ProductCategoryID == ProductCategoryId
+                        select u;
+            var categories = query.FirstOrDefault();
+            var model = new ProductCategoryModel()
+            {
+                ProductCategoryID = categories.ProductCategoryID,
+                Name = categories.Name,
+                ModifiedDate = categories.ModifiedDate          
+            };
+            return model;
+        }
+
+        public void InsertProductCategory(ProductCategoryModel ProductCategory)
+        {
+            var categoryData = new ProductCategory()
+            {
+                Name = ProductCategory.Name,
+                ModifiedDate = DateTime.Now,
+                rowguid = Guid.NewGuid()
+
+
+            };
+            _dataContext.ProductCategories.InsertOnSubmit(categoryData);
+            _dataContext.SubmitChanges();
+        }
+
+        public void UpdateProductCategory(ProductCategoryModel ProductCategory)
+        {
+            ProductCategory categoryData = _dataContext.ProductCategories.Where(u => u.ProductCategoryID == ProductCategory.ProductCategoryID).SingleOrDefault();
+            categoryData.Name = ProductCategory.Name;
+            categoryData.ModifiedDate = DateTime.Now;
+           
+            _dataContext.SubmitChanges();
+        }
+    }
+}
